01b5a7bf31bfdeacca981e57f22d5b6975cf2b53
em-wb <131539794+em-wb@users.noreply.github.com>
2023-10-06 13:52:51 +0200

    Edit: move dom variables Add: player name text input

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
 'use strict'
-const gameContainer = document.getElementById('game-container');
-const cells = gameContainer.querySelectorAll('.cell');
-const dialogBtn = document.getElementById('dialogBtn');
-const playerDialog = document.getElementById('player-dialog')
-const playerBtns = document.querySelectorAll('.player-btn')
-const submitBtn = document.getElementById('submit-btn')
-const choosePlayerOne = document.getElementById('player1')
-const choosePlayerTwo = document.getElementById('player2')
-const errorMsg = document.getElementById('error-msg')
-const winnerDialog = document.getElementById('winner-dialog')
-const winner = document.getElementById('winner')
-const restarts = document.querySelectorAll('.play-agn')
-
 
 // GAMEBOARD FUNCTION
 const gameBoard = (()=> {
@@ -28,7 +15,6 @@ const gameBoard = (()=> {
     function resetBoard() {
         console.log('resetboard')
         board = ['','','','','','','','','']
-        cells.forEach(cell=> cell.innerText='')
         return board
     }
 
@@ -44,7 +30,7 @@ const gameBoard = (()=> {
         [2, 4, 6],
         ]
 
-    //Add mark to game view, add mark to corresponding position in board array, call function to check for winners
+    //Add mark to game view, add mark to corresponding position in board array, call checkWinners
     function render(cell, mark, index) {
         if (mark!==undefined) {
         if(board[index]==='') {
@@ -56,7 +42,7 @@ const gameBoard = (()=> {
     }
     }
 
-    // Function to check for winners and call function to announce winners 
+    // Check for winners and call announceWinner 
     function checkWinners() {
         return winCombos.some(combo => {
         if (combo.every(pos => board[pos]==='x')) {
@@ -83,134 +69,165 @@ const gameBoard = (()=> {
 // PLAY GAME FUNCTION
 const game = (() => {
 
-// Create variables and event listener
+    // Create variables and event listeners
+        let playerOne = null
+        let playerTwo = null
+        let turn = true
 
-    let playerOne = null
-    let playerTwo = null
-    let turn = true
+        const cells = document.querySelectorAll('.cell');
+        const dialogBtn = document.getElementById('dialogBtn');
+        const playerDialog = document.getElementById('player-dialog')
+        const playerBtns = document.querySelectorAll('.player-btn')
+        const submitBtn = document.getElementById('submit-btn')
+        const choosePlayerOne = document.getElementById('player1')
+        const choosePlayerTwo = document.getElementById('player2')
+        const errorMsg = document.getElementById('error-msg')
+        const winnerDialog = document.getElementById('winner-dialog')
+        const winner = document.getElementById('winner')
+        const restarts = document.querySelectorAll('.play-agn')
+        const nameCtr = document.getElementById('.name-ctr')
+        const nameOne = document.getElementById('player1-name')
+        const nameTwo = document.getElementById('player2-name')
 
-    //Open dialog to choose players
-    dialogBtn.addEventListener('click', () => {
-        dialogBtn.style.animation = 'none';
-        playerDialog.showModal();
-    }),
 
-    //Player btns in choose player dialog
-    playerBtns.forEach(playerBtn =>
-        playerBtn.addEventListener("click", (e) =>{
-        choosePlayer(e)
-    }))
-
-    //Submit btn in choose player dialog
-    submitBtn.addEventListener('click', (e) =>{
-        submitPlayers(e)
-    })
-
-    // Restart gameplay btns
-    restarts.forEach(restart => {
-        restart.addEventListener('click', ()=>{
-            console.log('restart')
-            winnerDialog.close()
+        // Open dialog to choose players
+        dialogBtn.addEventListener('click', () => {
+            dialogBtn.style.animation = 'none';
+            resetPlay()
             gameBoard.resetBoard()
-            resetPlayers()
-            dialogBtn.style.animation = 'flash 3s infinite';
-        })
-    })
+            playerDialog.showModal();
+        }),
 
-    // User clicks a tictactoe cell during gameplay
-    cells.forEach(cell =>
-        cell.addEventListener('click', () => {
-            playTurn(cell)
+        // Choose player in playerDialog
+        playerBtns.forEach(playerBtn =>
+            playerBtn.addEventListener("click", (e) =>{
+            choosePlayer(e)
         }))
 
-    // Assign players based on user selection. Edit background colour of selected buttons to display user choice    
-    function choosePlayer(e) {
-        e.preventDefault()
-        e.target.style.backgroundColor = (e.target.style.backgroundColor === "#6B8E23") ? "#21482E" : "#6B8E23"
-        e.target.dataset.indexNumber = 1
-        let siblingBtn = Array.from(e.target.parentElement.children).find(child => child !== e.target)
-        if (siblingBtn) {
-        siblingBtn.style.backgroundColor = "#21482E";
-        siblingBtn.dataset.indexNumber = 0
-        }
-        console.log('p1', e.target.dataset.indexNumber)
-        console.log('p2',siblingBtn.dataset.indexNumber)
-    }
+        // Submit players in playerDialog
+        submitBtn.addEventListener('click', (e) =>{
+            submitPlayers(e)
+        })
 
-    // Called when submit btn clicked. Assigns players and marks, closes dialog
-    function submitPlayers(e) {
-        e.preventDefault()
-        gameBoard.resetBoard()
-        resetPlayers()
-        
-        if (choosePlayerOne.dataset.indexNumber==1){
-            playerOne = Player('Player 1', 'x')
-        } else if (choosePlayerOne.dataset.indexNumber==0){playerOne = Player('Bot 1', 'x')}
+        // Restart gameplay
+        restarts.forEach(restart => {
+            restart.addEventListener('click', ()=>{
+                console.log('restart')
+                winnerDialog.close()
+                gameBoard.resetBoard()
+                resetPlay()
+                dialogBtn.style.animation = 'flash 3s infinite';
+            })
+        })
+
+        // Users selection in tictactoe gameplay
+        cells.forEach(cell =>
+            cell.addEventListener('click', () => {
+                playTurn(cell)
+            }))
+    
+    //Gameplay functions
+    
+        // Assign players based on user selection. Edit background colour of selected buttons to display user choice    
+        function choosePlayer(e) {
+            e.preventDefault()
+            e.target.style.backgroundColor = (e.target.style.backgroundColor === "#6B8E23") ? "#21482E" : "#6B8E23"
+            e.target.dataset.indexNumber = 1
+            if (e.target.id == 'player1') {
+                nameOne.classList.remove('hidden')
+            } else if (e.target.id == 'player2') {
+                nameTwo.classList.remove('hidden')
+            } else if (e.target.id == 'bot1'){
+                nameOne.classList.add('hidden')
+            } else if (e.target.id == 'bot2'){
+                nameTwo.classList.add('hidden')
+            }
+            let siblingBtn = Array.from(e.target.parentElement.children).find(child => child !== e.target)
+            if (siblingBtn) {
+            siblingBtn.style.backgroundColor = "#21482E";
+            siblingBtn.dataset.indexNumber = 0
+            }
+            console.log('p1', e.target.dataset.indexNumber)
+            console.log('p2',siblingBtn.dataset.indexNumber)
+        }
+
+        // Called with submit btn in playerDiaolog. Assigns players and marks, closes dialog
+        function submitPlayers(e) {
+            e.preventDefault()
+            if (choosePlayerOne.dataset.indexNumber==1){
+                playerOne = (nameOne == '')? Player('Player 1', 'x') : Player(nameOne.value, 'x')
+            }else if (choosePlayerOne.dataset.indexNumber==0 ) playerOne = Player('Bot 1', 'x')
             console.log(playerOne)
 
-        if (choosePlayerTwo.dataset.indexNumber==1) {
-                playerTwo = Player('Player 2', 'o')
-        } else if (choosePlayerTwo.dataset.indexNumber==0){playerTwo = Player('Bot 2', 'x')}
-            console.log(playerTwo)
+            if (choosePlayerTwo.dataset.indexNumber==1) {
+                playerTwo = (nameOne == '')? Player('Player 2', 'x') : Player(nameTwo.value, 'x')      
+            } else if (choosePlayerTwo.dataset.indexNumber==0) playerTwo = Player('Bot 2', 'x')
+                console.log(playerTwo)
 
-        if (playerOne!==null && playerTwo!==null) {
-            playerDialog.close()
-            errorMsg.innerText = ""
-        } else {errorMsg.innerText = "*Please select players!"}
-        return {playerOne, playerTwo}
-    }
+            if (playerOne!==null && playerTwo!==null) {
+                playerDialog.close()
+                errorMsg.innerText = ""
+                gameBoard.resetBoard()
+                resetPlay()
+            } else {errorMsg.innerText = "*Please select players!"}
+            return {playerOne, playerTwo}
+        }
+        
+        // Get players
+        function getPlayerOne() {
+            return playerOne
+        }
 
-    // Get players
-    function getPlayerOne() {
-        return playerOne
-    }
-
-    function getPlayerTwo() {
-        return playerTwo
-    }
- 
-   
-    // Called when player clicks on a tictactoe cell. Alternates player turns and calls render OR calls endGame if board array is full 
-    function playTurn(cell) {
-        console.log(turn, "first")
-        const gameBoardArray = gameBoard.getBoard()
-        console.log(gameBoardArray)
-        const index = cell.id.slice(4,5)
-        console.log(index)
-        if (gameBoardArray.includes('')) {
-            if (turn===false){
-                turn=true
-                gameBoard.render(cell, playerTwo.mark, index)
-            }
-            else {
-                turn = false
-                gameBoard.render(cell, playerOne.mark, index)}
-        } else {endGame} 
-    }
+        function getPlayerTwo() {
+            return playerTwo
+        }
     
-    // Called when a player wins, or the board array is full. 
-    function announceWinner(result) {
-        let winName = "No one! It's a draw."
-        if (result == playerOne) {
-            winName = playerOne.name.toString()
+        // Called when player clicks on tictactoe cell. Alternates player turns and calls render OR endGame if board array is full 
+        function playTurn(cell) {
+            console.log(turn, "first")
+            const gameBoardArray = gameBoard.getBoard()
+            console.log(gameBoardArray)
+            const index = cell.id.slice(4,5)
+            console.log(index)
+            if (gameBoardArray.includes('')) {
+                if (turn===false){
+                    turn=true
+                    gameBoard.render(cell, playerTwo.mark, index)
+                }
+                else {
+                    turn = false
+                    gameBoard.render(cell, playerOne.mark, index)}
+            } else {endGame} 
         }
-        else if (result == playerTwo){
-            winName = playerTwo.name 
+        
+        // Called when a player wins OR the board array is full. 
+        function announceWinner(result) {
+            let winName = "No one! It's a draw."
+            if (result == playerOne) {
+                winName = playerOne.name.toString()
+            }
+            else if (result == playerTwo){
+                winName = playerTwo.name 
+            }
+            winner.innerText = winName
+            winnerDialog.showModal()
+        }    
+
+        // Reset gameplay: player allocation, turn allocation, playerBtns, tictactoe cells.
+        function resetPlay() {
+            playerOne = null
+            playerTwo = null
+            nameOne.value = ''
+            nameTwo.value = ''
+            turn = true
+            nameOne.classList.add('hidden')
+            nameTwo.classList.add('hidden')
+            playerBtns.forEach(playerBtn => {playerBtn.style.backgroundColor = "#21482E", playerBtn.dataset.indexNumber = 3})
+            cells.forEach(cell=> cell.innerText='')
         }
-        winner.innerText = winName
-        winnerDialog.showModal()
-    }    
-
-    // Reset player allocation, player turn allocation and player selection btns.
-    function resetPlayers() {
-        playerOne = null
-        playerTwo = null
-        turn = true
-        playerBtns.forEach(playerBtn => playerBtn.style.backgroundColor = "#21482E")
-    }
 
 
-    return {submitPlayers:submitPlayers, announceWinner: announceWinner, getPlayerOne:getPlayerOne, getPlayerTwo:getPlayerTwo} 
+        return {submitPlayers:submitPlayers, announceWinner: announceWinner, getPlayerOne:getPlayerOne, getPlayerTwo:getPlayerTwo} 
 
     
 })()
