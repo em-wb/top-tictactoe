ffc2c248a8f10500ffcf3d3fa8eccefa6d19677f
em-wb <131539794+em-wb@users.noreply.github.com>
2023-10-10 15:56:58 +0200

    Edit: remove bot v bot, improve random cell selection for bot-play.

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ const gameBoard = (()=> {
 
     //Reset board array and game view
     function resetBoard() {
-        console.log('resetboard')
         board = ['','','','','','','','','']
         return board
     }
@@ -31,11 +30,10 @@ const gameBoard = (()=> {
         ]
 
     //Add mark to game view, add mark to corresponding position in board array, call checkWinners
-    function render(cell, mark, index, type) {
+    function render(cell, mark, index) {
         if (mark!==undefined) {
             if(board[index]==='') {
             board[index]= mark
-            console.log('boardrender', board)
             cell.innerText = mark
             checkWinners()
             }
@@ -45,7 +43,6 @@ const gameBoard = (()=> {
 
     // Check for winners and call announceWinner 
     function checkWinners() {
-        console.log('wincheck')
         winCombos.some(combo => {
         if (combo.every(pos => board[pos]=='x')) {
             let playerOne = game.getPlayerOne()
@@ -53,13 +50,15 @@ const gameBoard = (()=> {
         } else if (combo.every(pos=> board[pos]=='o')) {
             let playerTwo = game.getPlayerTwo()
             return game.announceWinner(playerTwo)
-        } else if (combo.every(pos => board[pos]!=='x')&& combo.every(pos=> board[pos]!=='o') && !board.includes('')) {
+        }
+        if (!board.includes('')) {
             return game.announceWinner()
-        } else if (combo.every(pos => board[pos]!=='x')&& combo.every(pos=> board[pos]!=='o') && board.includes('')) {
-            setTimeout(getCheckBot,200)
+        } else {
+            setTimeout(getCheckBot,100)
         }
         })}
-    
+
+    //combo.every(pos => board[pos]!='x') && combo.every(pos=> board[pos]!='o') && 
     function getCheckBot() {
         return game.checkBot()
     }
@@ -77,18 +76,16 @@ const gameBoard = (()=> {
 const game = (() => {
 
     // Create variables and event listeners
-        let playerOne = null
+        let playerOne = 1
         let playerTwo = null
         let turn = true
 
-        
-
+    
         const cells = document.querySelectorAll('.cell');
         const dialogBtn = document.getElementById('dialogBtn');
         const playerDialog = document.getElementById('player-dialog')
         const playerBtns = document.querySelectorAll('.player-btn')
         const submitBtn = document.getElementById('submit-btn')
-        const choosePlayerOne = document.getElementById('player1')
         const choosePlayerTwo = document.getElementById('player2')
         const errorMsg = document.getElementById('error-msg')
         const winnerDialog = document.getElementById('winner-dialog')
@@ -121,7 +118,6 @@ const game = (() => {
         // Restart gameplay
         restarts.forEach(restart => {
             restart.addEventListener('click', ()=>{
-                console.log('restart')
                 winnerDialog.close()
                 gameBoard.resetBoard()
                 resetPlay()
@@ -142,46 +138,33 @@ const game = (() => {
             e.preventDefault()
             e.target.style.backgroundColor = (e.target.style.backgroundColor === "#6B8E23") ? "#21482E" : "#6B8E23"
             e.target.dataset.indexNumber = 1
-            if (e.target.id == 'player1') {
-                nameOne.classList.remove('hidden')
-            } else if (e.target.id == 'player2') {
-                nameTwo.classList.remove('hidden')
-            } else if (e.target.id == 'bot1'){
-                nameOne.classList.add('hidden')
-            } else if (e.target.id == 'bot2'){
-                nameTwo.classList.add('hidden')
-            }
+            e.target.id == 'player2' ? nameTwo.classList.remove('hidden') : nameTwo.classList.add('hidden')
             let siblingBtn = Array.from(e.target.parentElement.children).find(child => child !== e.target)
             if (siblingBtn) {
                 siblingBtn.style.backgroundColor = "#21482E";
                 siblingBtn.dataset.indexNumber = 0
             }
-            console.log('p1', e.target.dataset.indexNumber)
-            console.log('p2',siblingBtn.dataset.indexNumber)
         }
+
+        
 
         // Called with submit btn in playerDiaolog. Assigns players and marks, closes dialog
         function submitPlayers(e) {
             e.preventDefault()
-            if (choosePlayerOne.dataset.indexNumber==1){
-                playerOne = (nameOne.value === '')? Player('Player 1', 'x', 'human') : Player(nameOne.value, 'x', 'human')
-            }else if (choosePlayerOne.dataset.indexNumber==0 ) playerOne = Player('Bot 1', 'x', 'bot')
-            console.log(playerOne)
-
+            playerOne = (nameOne.value === '')? Player('Player 1', 'x', 'human') : Player(nameOne.value, 'x', 'human')
             if (choosePlayerTwo.dataset.indexNumber==1) {
                 playerTwo = (nameOne.value === '')? Player('Player 2', 'o', 'human') : Player(nameTwo.value, 'o', 'human')      
             } else if (choosePlayerTwo.dataset.indexNumber==0) playerTwo = Player('Bot 2', 'o', 'bot')
-                console.log(playerTwo)
-
-            if (playerOne!==null && playerTwo!==null) {
+            if (playerTwo!==null) {
                 playerDialog.close()
                 errorMsg.innerText = ""
                 gameBoard.resetBoard()
                 turnDisplay.innerText= "TURN: " + playerOne.name
-                setTimeout(checkBot,100)
-            } else {errorMsg.innerText = "*Please select players!"}
+            } else {errorMsg.innerText = "*Please select player 2 or bot!"}
             return {playerOne, playerTwo}
         }
+
+     
         
         // Get players
         function getPlayerOne() {
@@ -194,10 +177,8 @@ const game = (() => {
     
         // Called when player clicks on tictactoe cell. Alternates player turns and calls render OR endGame if board array is full 
         function playTurn(cell) {
-            console.log(turn)
             const index = cell.id.slice(4,5)
-            console.log(index)
-                if (turn === true && playerOne.type == 'human') {
+                if (turn === true) {
                     turn = false
                     gameBoard.render(cell, playerOne.mark, index)
                     turnDisplay.innerText="TURN: " + playerTwo.name
@@ -210,10 +191,7 @@ const game = (() => {
         }
 
         function checkBot() {
-            console.log('botcheck')
-            let gameBoardArray = gameBoard.getBoard()
-            if (!gameBoardArray.includes('')) gameBoard.checkWinners()
-            else if (playerOne.type=='bot' && turn == true || playerTwo.type=='bot' && turn == false) {
+            if (playerTwo.type=='bot' && turn == false) {
                 randomPickBot()
             }
             else return false
@@ -221,31 +199,20 @@ const game = (() => {
 
         
         function randomPickBot() {
-            let options = [0,1,2,3,4,5,6,7,8]
-            let randomI = options[Math.floor(Math.random() * options.length)];
-            console.log(randomI)
-            let botCell = cells[randomI]
-            botTurn(botCell, randomI)
-        }
-
-        function botTurn(botCell,randomI) {
-            const gameBoardArray = gameBoard.getBoard()
-            console.log('bot before',gameBoardArray)
-            if (gameBoardArray[randomI] !== '') randomPickBot()
-            else {
-                if (turn===true) {
-                    console.log('b1')
-                    turn=false
-                    turnDisplay.innerText="TURN: " + playerTwo.name
-                    gameBoard.render(botCell, playerOne.mark, randomI, playerOne.type)
-                } else if (turn==false) {
-                    console.log('b2')
-                    turn=true
-                    gameBoard.render(botCell, playerTwo.mark, randomI, playerTwo.type)
-                    turnDisplay.innerText="TURN: " + playerOne.name
+            turn=true
+            let gameBoardArray = gameBoard.getBoard()
+            let options = []
+            for (let i = 0; i < gameBoardArray.length; i++) {
+                if (gameBoardArray[i] === "") {
+                  options.push(i);
                 }
-            }}
-        
+            }
+            let randomI = options[Math.floor(Math.random() * options.length)];
+            let botCell = cells[randomI]
+            gameBoard.render(botCell, playerTwo.mark, randomI)
+            turnDisplay.innerText="TURN: " + playerOne.name
+        }
+     
         // Called when a player wins OR the board array is full. 
         function announceWinner(result) {
             let winName = "No one! It's a draw."
@@ -260,18 +227,16 @@ const game = (() => {
 
         // Reset gameplay: player allocation, turn allocation, playerBtns, tictactoe cells.
         function resetPlay() {
-            playerOne = null
+            playerOne = 1
             playerTwo = null
             nameOne.value = ''
             nameTwo.value = ''
             turn = true
-            nameOne.classList.add('hidden')
             nameTwo.classList.add('hidden')
             turnDisplay.innerText=''
             playerBtns.forEach(playerBtn => {playerBtn.style.backgroundColor = "#21482E", playerBtn.dataset.indexNumber = 3})
             cells.forEach(cell=> cell.innerText='')
         }
-
 
         return {submitPlayers:submitPlayers, announceWinner: announceWinner,
             getPlayerOne:getPlayerOne, getPlayerTwo:getPlayerTwo, randomPickBot:randomPickBot, checkBot:checkBot}
@@ -289,5 +254,3 @@ mark,
 type
 }
 }
-
-
